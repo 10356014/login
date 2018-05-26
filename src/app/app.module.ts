7a00d50b09f29e856a200a5a577de584e8679b30
lulu <10356014@ntub.edu.tw>
2018-05-26 17:28:12 +0800

    login+keyboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { KeyboardPage } from '../pages/keyboard/keyboard';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    KeyboardPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    KeyboardPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { KeyboardPage } from '../keyboard/keyboard';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public alertCtrl: AlertController) {
 
   }
-
+ 
+  doConfirm() {
+    let confirm = this.alertCtrl.create({
+      title: '提示',
+      message: '確認所選店鋪為台北市大同店?',
+      buttons: [
+        {
+          text: '返回',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: '確認',
+          handler: () => {
+            console.log('Agree clicked');
+            this.navCtrl.push(KeyboardPage);
+          }
+        }
+      ]
+    });
+    confirm.present()
+  }
+ 
 }
